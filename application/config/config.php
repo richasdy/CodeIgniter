5470aacdc6ecde149673c58d8cf8adef6bf7c5d6
RhalSahaja <62018942+RhalSahaja@users.noreply.github.com>
2020-03-23 06:36:49 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://localhost/TP7/TP_1301184057';
 
 /*
 |--------------------------------------------------------------------------
