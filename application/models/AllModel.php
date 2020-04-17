cc2cecd6ed627bd776f5f929d59acb79c4371788
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-17 11:55:57 +0700

    Add more method for model

--- application/models/AllModel.php
+++ application/models/AllModel.php
@@ -11,4 +11,25 @@ class AllModel extends CI_Model
 			  return false;
 		  }
     }
+    public function getPhoto()
+    {
+        $query = $this->db->get('photo');
+        $returnArray['num_rows'] = $query->num_rows();
+        $returnArray['result'] = $query->result();
+        return $returnArray;
+    }
+     public function getWhere($where, $table)
+    {
+        return $this->db->get_where($table, $where);
+    }
+    public function cari($keyword)
+    {
+        $this->db->where('caption', $keyword);
+        $query = $this->db->get('photo');
+        if($query->num_rows() > 0){
+			    return true;
+		    }else{
+			    return false;
+		    }
+    }
 }
