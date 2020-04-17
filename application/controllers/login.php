55a9d4b724065dd4b36b9a07a34546721a168945
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-17 12:02:27 +0700

    Add method login

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -2,25 +2,32 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
-
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	
-		public function index()
-		{
-			$this->load->view('view_login');
-		}
+ function __construct(){
+  	parent::__construct();
+  	$this->load->model('AllModel');
+ }
+ 
+ public function index()
+ {
+  	$this->load->view('view_login');
+  	$this->load->view('templates/footer');
+ }
+ 
+ public function login(){
+  #call login() function (the one that's in the model) and check the result
+  #if true set session and fill it with the inputted username and redirect to '/profile'
+  #if not true show error message
+    $data['username'] = $this->input->post('username');
+    $data['password'] = $this->input->post('password');
+    if($this->m_id->login($data)) {
+   	  $this->session->set_userdata('username', $this->input->post('username'));
+   	  $this->session->set_userdata('password', $this->input->post('password'));
+      redirect('/feed');
+    } else {
+      $data['error_message'] = "Username or Password ain't correct";
+			$this->load->view('view_login', $data);
+			$this->load->view('templates/footer');
+    }
+ }
 }
+?>
