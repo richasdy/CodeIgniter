96a5f55aeeac5bacfd6a207b45e9af7b22531aea
rezaayahya <rezaahmadyahya@gmail.com>
2020-04-17 11:44:17 +0700

    Edit m_id to AllModel

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -13,13 +13,11 @@ class Login extends CI_Controller {
   	$this->load->view('templates/footer');
  }
  
- public function login(){
-  #call login() function (the one that's in the model) and check the result
-  #if true set session and fill it with the inputted username and redirect to '/profile'
-  #if not true show error message
+ public function login()
+ {
     $data['username'] = $this->input->post('username');
     $data['password'] = $this->input->post('password');
-    if($this->m_id->login($data)) {
+    if($this->AllModel->login($data)) {
    	  $this->session->set_userdata('username', $this->input->post('username'));
    	  $this->session->set_userdata('password', $this->input->post('password'));
       redirect('/feed');
