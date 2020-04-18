e43b2143c3111df200af58edd09dabc282db4176
izzanfr <izzanfaikar3@gmail.com>
2020-04-18 17:40:20 +0700

    Tugas CI_Instagram-1301184216

--- application/views/view_login.php
+++ application/views/view_login.php
@@ -1,30 +1,37 @@
 <!DOCTYPE html>
 <html>
     <?php 
-         
+
     ?>
-    <head>
+    <head> 
         <title>Vietgram | Login</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="description" content="Vietgram, like Instagram but with Pho" />
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/styles.css' ?>" />
-        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap.min.css' ?>" />
     </head>
     <body>
         <main id="login">
+        
             <div class="login__column">
-                <img src="<?php echo base_url().'assets/images/phoneImage.png' ?>" class="login__phone" />
+                <img src="assets/images/phoneImage.png" class="login__phone" />
             </div>
             <div class="login__column">
                 <div class="login__box">
-                    <img src="<?php echo base_url().'assets/images/loginLogo.png' ?>" class="login__logo" />
-                    <form action="feed.html" method="get" class="login__form">
+                    <img src="assets/images/loginLogo.png" class="login__logo" />
+                    <?php if(isset($error_message)) { ?>
+                        <div class="alert alert-danger" role="alert">
+                            <?= $error_message ?>
+                        </div>
+                    <?php 
+                        } 
+                    ?>
+                    <form action="<?php echo base_url('Controller_login/operasi_login'); ?>" method="post" class="login__form">
                         <input type="text" name="username" placeholder="Username" required />
                         <input type="password" name="password" placeholder="Password" required />
                         <input type="submit" value="Log in" />
@@ -42,8 +49,8 @@
                 <div class="login__box--transparent">
                     <span>Get the app.</span>
                     <div class="login__appstores">
-                        <img src="<?php echo base_url().'assets/images/ios.png' ?>" class="login__appstore" alt="Apple appstore logo" title="Apple appstore logo" />
-                        <img src="<?php echo base_url().'assets/images/android.png' ?>" class="login__appstore" alt="Android appstore logo" title="Android appstore logo" />
+                        <img src="assets/images/ios.png" class="login__appstore" alt="Apple appstore logo" title="Apple appstore logo" />
+                        <img src="assets/images/android.png" class="login__appstore" alt="Android appstore logo" title="Android appstore logo" />
                     </div>
                 </div>
             </div>
