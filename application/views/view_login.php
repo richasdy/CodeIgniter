6a663e70b4afbd5fe35aa050aec1909ebfa1dec0
Fahmihamzah84 <fahmihamzah84@gmail.com>
2020-04-18 23:31:52 +0700

    fix_autoload

--- application/views/view_login.php
+++ application/views/view_login.php
@@ -7,16 +7,20 @@
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <meta name="description" content="Vietgram, like Instagram but with Pho" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="<?php echo base_url('assets/css/styles.css') ?>">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.33.1/sweetalert2.min.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/styles.css' ?>" />
+        <link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/bootstrap.min.css' ?>" />
     </head>
     <body>
         <main id="login">
             <div class="login__column">
-                <img src="<?php echo base_url('assets/images/phoneImage.png'); ?>" class="login__phone" />
+                <img src="<?php echo base_url().'assets/images/phoneImage.png' ?>" class="login__phone" />
             </div>
             <div class="login__column">
                 <div class="login__box">
-                    <img src="<?php echo base_url('assets/images/loginLogo.png'); ?>" class="login__logo" />
+                    <img src="<?php echo base_url().'assets/images/loginLogo.png' ?>" class="login__logo" />
                     <form action="feed.html" method="get" class="login__form">
                         <input type="text" name="username" placeholder="Username" required />
                         <input type="password" name="password" placeholder="Password" required />
@@ -35,8 +39,8 @@
                 <div class="login__box--transparent">
                     <span>Get the app.</span>
                     <div class="login__appstores">
-                        <img src="<?php echo base_url('assets/images/ios.png'); ?>" class="login__appstore" alt="Apple appstore logo" title="Apple appstore logo" />
-                        <img src="<?php echo base_url('assets/images/android.png'); ?>" class="login__appstore" alt="Android appstore logo" title="Android appstore logo" />
+                        <img src="<?php echo base_url().'assets/images/ios.png' ?>" class="login__appstore" alt="Apple appstore logo" title="Apple appstore logo" />
+                        <img src="<?php echo base_url().'assets/images/android.png' ?>" class="login__appstore" alt="Android appstore logo" title="Android appstore logo" />
                     </div>
                 </div>
             </div>
@@ -63,4 +67,4 @@
             </div>
         </footer>
     </body>
-</html> 
+</html>
