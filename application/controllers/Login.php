8acb9471b48419f25dbd694f5959d354cdc409a8
FirlisaAnggraeni <anggraenifirlisa@gmail.com>
2020-04-18 13:47:39 +0700

    Vietgram Code Igniter

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,25 +1,27 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('view_login');
-	}
+    function __construct(){
+        parent::__construct();
+        $this->load->model('User');
+    }
+
+    public function index() {
+        $this->load->view('view_login');
+    }
+
+    public function login(){
+        $data['username'] = $this->input->post('username');
+        $data['password'] = $this->input->post('password');
+        $result = $this->User->login($data);
+        if ($result) {
+            $this->session->set_userdata('id',$result->id_user);
+            $this->session->set_userdata('user',$data['username']);
+            redirect('/feed');
+        } else {
+            $error = array('error_message' => "Username or Password ain't correct");
+            $this->load->view('view_login', $error);
+        }
+    }
 }
